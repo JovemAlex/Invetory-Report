0f0afa2ed9e2924b1611b08b1edad04828db6040
Alex <alexsandrolima991@hotmail.com>
2023-04-04 19:08:46 -0300

    (feat): implements complete_report

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,17 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+    @classmethod
+    def generate(cls, list):
+        counter = Counter([p["nome_da_empresa"] for p in list])
+        simple_report = SimpleReport.generate(list)
+        companies = ""
+
+        for company, quantity in counter.items():
+            companies += f"- {company}: {quantity}\n"
+
+        return f"""{simple_report}
+Produtos estocados por empresa:
+{companies}"""
